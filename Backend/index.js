ac7809fc00b72d2b4f65f676377987c9517a2ecb
Jesus Manuel Urias Castro <maxivale@MacBook-Pro-de-Jesus-2.local>
2026-05-09 13:14:12 -0700

    index.js

--- Backend/index.js
+++ Backend/index.js
@@ -40,23 +40,88 @@ app.get('/api/productos', async (req, res) => {
 });
 
 app.get('/api/productos/:id', async (req, res) => {
-  const id = Number(req.params.id);
+  try {
+    const id = Number(req.params.id);
 
-  const producto = productos.find(u => u.id === id);
+    const [rows] = await pool.query(
+      `
+      SELECT 
+        id,
+        nombre,
+        categoria,
+        stock,
+        precio,
+        creado_en
+      FROM productos
+      WHERE id = ?
+      `,
+      [id]
+    );
 
-  if(!producto) {
-    return res.status(404).json({
-      mensaje:'Producto no encontrado'
-    })
+    if (rows.length === 0) {
+      return res.status(404).json({
+        mensaje: 'Usuario no encontrado'
+      });
+    }
+    const producto = new ProductoModel(rows[0]);
+
+    res.json({
+      mensaje:'Producto encontrado',
+      data: producto
+    });
+  } catch (error) {
+    res.status(500).json({
+      mensaje: 'Error al consultar producto',
+      error: error.message
+    });
   }
-  res.json({
-    mensaje:'Producto encontrado',
-    data: producto
-  });
 });
 
-app.post('/api/productos', (req, res) => {
-  console.log('Crear nuevo producto');
+app.post('/api/usuarios', async (req, res) => {
+
+  try {
+    const { nombre, categoria, stock, precio } = req.body;
+    if (!nombre || !categoria || !stock || !precio) {
+      return res.status(400).json({
+        mensaje: 'Todos los campos son obligatorios'
+      });
+    }
+
+    const [result] = await pool.query(
+      `
+      INSERT INTO productos 
+        (nombre, categoriSa, stock, precio, creado_en)
+      VALUES 
+        (?, ?, ?, ?, NOW())
+      `,
+      [nombre, categoria, stock, precio]
+    );
+
+    const [rows] = await pool.query(
+      `
+      SELECT 
+        id,
+        nombre,
+        categoria,
+        stock,
+        precio,
+        creado_en
+      FROM productos
+      WHERE id = ?
+      `,
+      [result.insertId]
+    );
+    const productoCreado = new ProductoModel(rows[0]);
+    res.status(201).json({
+      mensaje: 'Producto creado correctamente',
+      data: productoCreado
+    });
+  } catch (error) {
+    res.status(500).json({
+      mensaje: 'Error al crear producto',
+      error: error.message
+    });
+  }
 });
 
 app.put('/api/productos/:id', (req, res) => {
